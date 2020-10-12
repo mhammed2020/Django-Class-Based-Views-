a0fc9b6d06560c02cdf07d6465753017569c26a4
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-10-12 18:20:48 +0100

    manage static files and media settings

--- project/settings.py
+++ project/settings.py
@@ -119,3 +119,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+   
+]
+
+MEDIA_ROOT =  BASE_DIR / "media"
+MEDIA_URL = '/media/'
+
+
