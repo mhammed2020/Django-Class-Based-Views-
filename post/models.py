a07b569022630238eae638bb59ec78df67d6ac58
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-10-12 17:54:00 +0100

    create post App and Post Model

---
+++ post/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.utils import timezone
+# Create your models here.
+
+class Post(models.Model):
+
+    title = models.CharField(max_length=200, blank=True, null=True)
+    content = models.TextField(max_length=2000,blank=True, null=True)
+    created_at = models.DateTimeField(default = timezone.now)
+    image = models.ImageField( upload_to='post-img/')
+
+    def __str__(self):
+        return self.title
+
+   
